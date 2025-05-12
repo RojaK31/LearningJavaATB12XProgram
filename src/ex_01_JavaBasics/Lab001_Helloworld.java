f81f40769ac54607dcc190e94a111830ef75175a
RojaK31 <k.rojagomathyaiswarya@gmail.com>
2025-05-12 10:37:09 +0530

    Hi

--- src/ex_01_JavaBasics/Lab001_Helloworld.java
+++ src/ex_01_JavaBasics/Lab001_Helloworld.java
@@ -1,4 +1,8 @@
 package ex_01_JavaBasics;
 
 public class Lab001_Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hi,This is my first java program");
+    }
+
 }
